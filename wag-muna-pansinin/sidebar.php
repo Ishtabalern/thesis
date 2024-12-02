03181fb2f91d63a2bc07e9534d2b3eb744fb807d
Ishtabalern <ivojohnmbarroba@gmail.com>
2024-12-02 16:14:51 +0800

    4:14 pm
    spring cleaning

---
+++ wag-muna-pansinin/sidebar.php
@@ -0,0 +1,18 @@
+<head>
+<link rel="stylesheet" href="sidebar.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<section class="sidebar">
+        <div class="company-logo">
+            <img src="../imgs/csk_logo.png" alt="">
+        </div>
+        <div class="btn-container">
+            <button class="btn-tabs" data-tab="home"><i class="fa-solid fa-house"></i> Home</button>
+            <button class="btn-tabs" href="scan.php"><i class="fa-solid fa-camera"></i> Capture Documents</a>
+            <button class="btn-tabs" href="records.php"><i class="fa-solid fa-file"></i> Financial Records</a>
+            <button class="btn-tabs" href="generateReport-employee.php"><i class="fa-solid fa-file-export"></i> Generate Report</a>
+            <button class="btn-tabs" data-tab="manage-users"><i class="fa-solid fa-users-gear"></i> Manage Users</button>
+            <button class="btn-tabs" data-tab="system-logs"><i class="fa-solid fa-file-circle-check"></i> Audit Logs</button>
+            <button class="btn-tabs" data-tab="settings"><i class="fa-solid fa-gear"></i> Settings</button>
+        </div>
+    </section>
